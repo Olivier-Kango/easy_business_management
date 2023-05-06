2512a25ca42fbe24a14f5ebf7467302cdeae3bb2
Olivier-Kango <olivierkango@gmail.com>
2023-05-06 11:51:51 +0300

    Add confirmation controller

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,27 @@
+class ConfirmationsController < ApplicationController
+
+  def create
+    @user = User.find_by(email: params[:user][:email].downcase)
+
+    if @user.present? && @user.unconfirmed?
+      redirect_to root_path, notice: "Check your email for confirmation instructions."
+    else
+      redirect_to new_confirmation_path, alert: "We could not find a user with that email or that email has already been confirmed."
+    end
+  end
+
+  def edit
+    @user = User.find_signed(params[:confirmation_token], purpose: :confirm_email)
+
+    if @user.present?
+      @user.confirm!
+      redirect_to root_path, notice: "Your account has been confirmed."
+    else
+      redirect_to new_confirmation_path, alert: "Invalid token."
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  # devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_pages#home"
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
 end
