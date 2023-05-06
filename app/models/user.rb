54168c72d85a8e099dd1efd9d1daf9255e8d1361
Olivier-Kango <olivierkango@gmail.com>
2023-05-06 11:36:48 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,23 @@
 class User < ApplicationRecord
+  CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+
+  has_secure_password
+
   before_save :downcase_email
 
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+
+  def confirm!
+    update_columns(confirmed_at: Time.current)
+  end
+
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def unconfirmed?
+    !confirmed?
+  end
 
   private
 
