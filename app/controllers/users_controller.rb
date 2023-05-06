694f8b3c6c31bfccba490ae3bc3f7f3a94f022f2
Olivier-Kango <olivierkango@gmail.com>
2023-05-06 11:45:27 +0300

    Add UsersController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Please check your email for confirmation instructions."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
