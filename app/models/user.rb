2e76f39737fdd4d3d2b0b35d1b2ede3235b33da8
Olivier-Kango <olivierkango@gmail.com>
2023-05-06 10:19:40 +0300

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
