af1fa40cf77b3d88e9afbaae664c2dfec300cf57
Olivier-Kango <olivierkango@gmail.com>
2023-05-06 11:53:28 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root "static_pages#home"
   post "sign_up", to: "users#create"
   get "sign_up", to: "users#new"
+
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
 end
