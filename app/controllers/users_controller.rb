ccaafb659e8f5dc138bf5192d47fed3991099e68
Olivier-Kango <olivierkango@gmail.com>
2023-05-07 16:25:23 +0300

    Login user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :redirect_if_authenticated, only: [:create, :new]
   def create
     @user = User.new(user_params)
     if @user.save
