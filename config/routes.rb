ab3e54356078f9259221fea0769ed5436f87ff52
Olivier-Kango <olivierkango@gmail.com>
2023-05-07 16:21:30 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   post 'sign_up', to: 'users#create'
   get 'sign_up', to: 'users#new'
 
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
+
   resources :confirmations, only: %i[create edit new], param: :confirmation_token
 end
