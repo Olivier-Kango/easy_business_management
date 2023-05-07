435d0b04700abc31cdfab3c710a9e860c2bdece7
Olivier-Kango <olivierkango@gmail.com>
2023-05-07 16:26:38 +0300

    Update User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,15 @@ class User < ApplicationRecord
     UserMailer.confirmation(self, confirmation_token).deliver_now
   end
 
+  def generate_password_reset_token
+    signed_id expires_in: PASSWORD_RESET_TOKEN_EXPIRATION, purpose: :reset_password
+  end
+
+  def send_password_reset_email!
+    password_reset_token = generate_password_reset_token
+    UserMailer.password_reset(self, password_reset_token).deliver_now
+  end
+
   private
 
   def downcase_email
