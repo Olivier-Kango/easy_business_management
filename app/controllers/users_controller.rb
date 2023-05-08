dbfe637ec8f11abe7d36826c9c08df998beb2a5d
Olivier-Kango <olivierkango@gmail.com>
2023-05-08 19:57:16 +0300

    Add destroy, edit and update methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :destroy, :update]
   before_action :redirect_if_authenticated, only: [:create, :new]
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,9 +18,46 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def destroy
+    current_user.destroy
+    reset_session
+    redirect_to root_path, notice: "Your account has been deleted."
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.authenticate(params[:user][:current_password])
+      if @user.update(update_user_params)
+        if params[:user][:unconfirmed_email].present?
+          @user.send_confirmation_email!
+          redirect_to root_path, notice: "Check your email for confirmation instructions."
+        else
+          redirect_to root_path, notice: "Account updated."
+        end
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    else
+      flash.now[:error] = "Incorrect password"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def create_user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def update_user_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation, :unconfirmed_email)
   end
 end
