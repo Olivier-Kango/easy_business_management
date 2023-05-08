35bb8771b32b5bee26f070dd82080a0226d4067f
Olivier-Kango <olivierkango@gmail.com>
2023-05-08 19:52:36 +0300

    Update confirmation controller

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -16,12 +16,17 @@ class ConfirmationsController < ApplicationController
     @user = User.find_signed(params[:confirmation_token], purpose: :confirm_email)
 
     if @user.present?
-      @user.confirm!
-      login @user
-      redirect_to root_path, notice: 'Your account has been confirmed.'
+      if @user.confirm!
+        login @user
+        redirect_to root_path, notice: "Your account has been confirmed."
+      else
+        redirect_to new_confirmation_path, alert: "Something went wrong."
+      end
     else
       redirect_to new_confirmation_path, alert: 'Invalid token.'
     end
+
+
   end
 
   def new
