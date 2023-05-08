4e112388315047741c9e4d59249688d5ae65b506
Olivier-Kango <olivierkango@gmail.com>
2023-05-08 19:50:23 +0300

    Update User Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,7 @@ class UserMailer < ApplicationMailer
     @confirmation_token = confirmation_token
 
     mail to: @user.email, subject: 'Confirmation Instructions'
+    mail to: @user.confirmable_email, subject: "Confirmation Instructions"
   end
 
   def password_reset(user, password_reset_token)
