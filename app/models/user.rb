5e1dcf53a7d5b9a4a642e482b47e65aabde2e01b
Olivier-Kango <olivierkango@gmail.com>
2023-05-08 19:48:54 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,31 @@
 class User < ApplicationRecord
   CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
   MAILER_FROM_EMAIL = 'no-reply@example.com'
+  attr_accessor :current_password
 
   has_secure_password
 
   before_save :downcase_email
 
+  before_save :downcase_unconfirmed_email
+
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+
+  validates :unconfirmed_email, format: {with: URI::MailTo::EMAIL_REGEXP, allow_blank: true}
 
   def confirm!
     update_columns(confirmed_at: Time.current)
+  end
+
+  def confirm!
+    if unconfirmed_or_reconfirming?
+      if unconfirmed_email.present?
+        return false unless update(email: unconfirmed_email, unconfirmed_email: nil)
+      end
+      update_columns(confirmed_at: Time.current)
+    else
+      false
+    end
   end
 
   def confirmed?
@@ -27,6 +43,14 @@ class User < ApplicationRecord
     UserMailer.confirmation(self, confirmation_token).deliver_now
   end
 
+  def confirmable_email
+    if unconfirmed_email.present?
+      unconfirmed_email
+    else
+      email
+    end
+  end
+
   def generate_password_reset_token
     signed_id expires_in: PASSWORD_RESET_TOKEN_EXPIRATION, purpose: :reset_password
   end
@@ -36,9 +60,22 @@ class User < ApplicationRecord
     UserMailer.password_reset(self, password_reset_token).deliver_now
   end
 
+  def reconfirming?
+    unconfirmed_email.present?
+  end
+
+  def unconfirmed_or_reconfirming?
+    unconfirmed? || reconfirming?
+  end
+
   private
 
   def downcase_email
     self.email = email.downcase
+  end
+
+  def downcase_unconfirmed_email
+    return if unconfirmed_email.nil?
+    self.unconfirmed_email = unconfirmed_email.downcase
   end
 end
