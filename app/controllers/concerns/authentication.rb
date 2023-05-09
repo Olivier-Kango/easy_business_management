c2c6c9e46f5a27b47ec577ec999ddb65c2dadca7
Olivier-Kango <olivierkango@gmail.com>
2023-05-09 19:27:08 +0300

    Update Authentication Concern

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -18,6 +18,7 @@ module Authentication
   end
 
   def authenticate_user!
+    store_location
     redirect_to login_path, alert: "You need to login to access that page." unless user_signed_in?
   end
 
@@ -46,6 +47,10 @@ module Authentication
 
   def user_signed_in?
     Current.user.present?
+  end
+
+  def store_location
+    session[:user_return_to] = request.original_url if request.get? && request.local?
   end
 
 end
