bdf7430041388ea61bb6aa1e0efc211f5aac844f
Olivier-Kango <olivierkango@gmail.com>
2023-05-09 21:49:40 +0300

    Add new columns to the active_sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_163350) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_09_165627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_09_163350) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.string "ip_address"
     t.index ["user_id"], name: "index_active_sessions_on_user_id"
   end
 
