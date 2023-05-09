df711254e1cd444af1450e50bb50a23805636cd9
Olivier-Kango <olivierkango@gmail.com>
2023-05-09 19:45:01 +0300

    Update the migration

--- db/migrate/20230509163350_create_active_sessions.rb
+++ db/migrate/20230509163350_create_active_sessions.rb
@@ -1,7 +1,7 @@
 class CreateActiveSessions < ActiveRecord::Migration[7.0]
   def change
     create_table :active_sessions do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: {on_delete: :cascade}
 
       t.timestamps
     end
