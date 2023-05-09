5051cca2b360239c368b54d3bcab633775c60d94
Olivier-Kango <olivierkango@gmail.com>
2023-05-09 22:55:56 +0300

    Refactor Active Sessions Controller

--- app/controllers/active_sessions_controller.rb
+++ app/controllers/active_sessions_controller.rb
@@ -9,12 +9,14 @@ class ActiveSessionsController < ApplicationController
     if current_user
       redirect_to account_path, notice: "Session deleted."
     else
+      forget_active_session
       reset_session
       redirect_to root_path, notice: "Signed out."
     end
   end
 
   def destroy_all
+    forget_active_session
     current_user.active_sessions.destroy_all
     reset_session
 
