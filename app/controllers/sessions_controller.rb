71d2e31115f149353b2caae330a1bb1d5d143846
Olivier-Kango <olivierkango@gmail.com>
2023-05-09 19:24:34 +0300

    Update create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   before_action :redirect_if_authenticated, only: [:create, :new]
 
+  before_action :authenticate_user!, only: [:destroy]
+
   def create
     @user = User.find_by(email: params[:user][:email].downcase)
     if @user
@@ -8,6 +10,7 @@ class SessionsController < ApplicationController
         redirect_to new_confirmation_path, alert: "Incorrect email or password."
       elsif @user.authenticate(params[:user][:password])
         login @user
+        remember(@user) if params[:user][:remember_me] == "1"
         redirect_to root_path, notice: "Signed in."
       else
         flash.now[:alert] = "Incorrect email or password."
@@ -20,6 +23,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    forget(current_user)
     logout
     redirect_to root_path, notice: "Signed out."
   end
