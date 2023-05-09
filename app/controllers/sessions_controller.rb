407e02cc7e4671d1ea70608b56c55660e8a60cba
Olivier-Kango <olivierkango@gmail.com>
2023-05-09 19:28:45 +0300

    Update Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       if @user.unconfirmed?
         redirect_to new_confirmation_path, alert: "Incorrect email or password."
       elsif @user.authenticate(params[:user][:password])
+        after_login_path = session[:user_return_to] || root_path
         login @user
         remember(@user) if params[:user][:remember_me] == "1"
         redirect_to root_path, notice: "Signed in."
